bb01f4b7d08b046ef49d0bf770839a22fe41e027
kishikishore2863 <kishikishore.2863@gmail.com>
2025-11-03 23:35:45 +0530

    testcode

---
+++ src/Rank/Program.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Kishi Kishore N on 03/11/25.
+//
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float f;
+    double d;
+    cin>>a>>b>>c>>f>>d;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<f<<endl;
+    cout<<d<<endl;
+    return 0;
+}
