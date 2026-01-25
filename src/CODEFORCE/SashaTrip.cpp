d46c946b2ac3621c7df7a44fcaebaa4d9182e796
kishikishore2863 <kishikishore.2863@gmail.com>
2026-01-25 23:10:07 +0530

    unit4

---
+++ src/CODEFORCE/SashaTrip.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Kishi Kishore N on 24/01/26.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,v;
+    cin>>n>>v;
+    int tank = min(n,v);
+
+    return 0;
+}
+
+
+
