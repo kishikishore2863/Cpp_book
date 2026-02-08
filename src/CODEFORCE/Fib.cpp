fe5feceb8bf1cf47309ba2bb9cdd7da4b18b2104
kishikishore2863 <kishikishore.2863@gmail.com>
2026-02-09 00:04:02 +0530

    updated

--- src/CODEFORCE/Fib.cpp
+++ src/CODEFORCE/Fib.cpp
@@ -5,6 +5,44 @@
 #include <vector>
 using namespace std;
 
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int a1, a2, a4, a5;
+//         cin >> a1 >> a2 >> a4 >> a5;
+//
+//         // Possible values for a3 derived from the equations
+//         vector<int> candidates = {
+//             a1 + a2,
+//             a4 - a2,
+//             a5 - a4
+//         };
+//
+//         int ans = 0;
+//
+//         for (int a3 : candidates) {
+//             int cnt = 0;
+//             if (a3 == a1 + a2) cnt++;       // i = 1
+//             if (a4 == a2 + a3) cnt++;       // i = 2
+//             if (a5 == a3 + a4) cnt++;       // i = 3
+//             ans = max(ans, cnt);
+//         }
+//
+//         cout << ans << "\n";
+//     }
+//     return 0;
+// }
+
+
+#include<iostream>
+
+
+using namespace std;
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -12,27 +50,12 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int a1, a2, a4, a5;
-        cin >> a1 >> a2 >> a4 >> a5;
+        long long a, b;
+        cin >> a >> b;
 
-        // Possible values for a3 derived from the equations
-        vector<int> candidates = {
-            a1 + a2,
-            a4 - a2,
-            a5 - a4
-        };
-
-        int ans = 0;
-
-        for (int a3 : candidates) {
-            int cnt = 0;
-            if (a3 == a1 + a2) cnt++;       // i = 1
-            if (a4 == a2 + a3) cnt++;       // i = 2
-            if (a5 == a3 + a4) cnt++;       // i = 3
-            ans = max(ans, cnt);
-        }
-
-        cout << ans << "\n";
+        long long r = a % b;
+        if (r == 0) cout << 0 << "\n";
+        else cout << (b - r) << "\n";
     }
     return 0;
 }
---
+++ src/CODEFORCE/demo.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Kishi Kishore N on 07/02/26.
+//
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    for (int i=0; i<n; i++) {
+        int a,b;
+        cin>>a>>b;
+        int count =0;
+        while (a%b == 0) {
+            a++;
+            count++;
+        }
+        cout<<count<<endl;
+    }
+    return 0;
+}
